296328b33a635ba1814534f1ec990817a504eee2
Vunas <vuh265@gmail.com>
2025-04-03 22:50:15 +0700

    fix loi vat

--- BE/src/main/java/com/app/QLCH/utils/FileCleanupJob.java
+++ BE/src/main/java/com/app/QLCH/utils/FileCleanupJob.java
@@ -1,6 +1,7 @@
 package com.app.QLCH.utils;
 
 import java.io.File;
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,6 +9,7 @@ import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 import com.app.QLCH.repository.NguyenLieuRepository;
+import com.app.QLCH.repository.SanPhamRepository;
 
 import jakarta.annotation.PostConstruct;
 
@@ -16,6 +18,8 @@ public class FileCleanupJob {
 
     @Autowired
     private NguyenLieuRepository nguyenLieuRepository;
+    @Autowired
+    private SanPhamRepository sanPhamRepository;
 
     @Scheduled(cron = "0 0 0 * * ?") // Chạy hàng ngày lúc 00:00
     public void cleanUnusedFiles() {
@@ -32,10 +36,9 @@ public class FileCleanupJob {
         File[] files = uploadDir.listFiles();
 
         if (files != null) {
-            List<String> usedFiles = nguyenLieuRepository.findAllImagePaths();
-            for (String string : usedFiles) {
-                System.out.println(string);
-            }
+            List<String> usedFiles = new ArrayList<>();
+            usedFiles.addAll(nguyenLieuRepository.findAllImagePaths());
+            usedFiles.addAll(sanPhamRepository.findAllImagePaths());
 
             for (File file : files) {
                 if (!usedFiles.stream().anyMatch(item -> item.contains(file.getName().trim()))) {
@@ -46,4 +49,3 @@ public class FileCleanupJob {
         }
     }
 }
-
